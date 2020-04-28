6a30d990695e31670afcf9b75091be1d3d8c9a8e
Michael RP <mikaelphang@gmail.com>
2020-04-28 18:07:07 +0700

    service locator

--- Xyz/GameTest/TicTacToeTest.cs
+++ Xyz/GameTest/TicTacToeTest.cs
@@ -3,10 +3,18 @@ using Xunit;
 
 using System.Collections.Generic;
 
+using Xyz.Game.ExpGainer;
+
 namespace Xyz.Game.Test
 {
   public class TicTacToeTest
   {
+    public TicTacToeTest()
+    {
+      ServiceLocator.Instance().Register("tictactoe_win", new TicTacToeWin());
+      ServiceLocator.Instance().Register("tictactoe_lose", new TicTacToeLose());
+    }
+
     [Fact]
     public void InvalidPlayers()
     {
@@ -164,6 +172,38 @@ namespace Xyz.Game.Test
       Assert.True(isEnded);
 
       Assert.Equal(5, amir.Exp);
+      Assert.Equal(2, budi.Exp);
+    }
+
+    [Fact]
+    public void TestMultiplier()
+    {
+      ServiceLocator.Instance().Register("tictactoe_win",
+        new Multiplier(new TicTacToeWin(), 2)
+      );
+
+      List<User> users = new List<User>();
+
+      User amir = User.NewUser("Amir");
+      User budi = User.NewUser("Budi");
+
+      users.Add(amir);
+      users.Add(budi);
+
+      XyzGame game = new TicTacToe(users);
+
+      Assert.Equal("tic-tac-toe", game.Name());
+
+      game.Move(new TicTacToeMove(game, amir, 2));
+      game.Move(new TicTacToeMove(game, budi, 3));
+
+      game.Move(new TicTacToeMove(game, amir, 4));
+      game.Move(new TicTacToeMove(game, budi, 0));
+
+      bool isEnded = game.Move(new TicTacToeMove(game, amir, 6));
+      Assert.True(isEnded);
+
+      Assert.Equal(10, amir.Exp);
       Assert.Equal(2, budi.Exp);
     }
   }

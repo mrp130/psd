4fbf8288172343b6ed0a84597c29e5599a739dd3
Michael RP <mikaelphang@gmail.com>
2020-06-06 23:48:18 +0700

    create table query

---
+++ Xyz/create_table.sql
@@ -0,0 +1,54 @@
+CREATE TABLE "room"
+(
+    id          UUID PRIMARY KEY,
+    max_player  INT NOT NULL,
+    game_type   VARCHAR(100),
+    game_config JSONB,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ,
+    deleted_at  TIMESTAMPTZ
+);
+
+CREATE TABLE "user"
+(
+    id         UUID PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE "exp"
+(
+    id         UUID PRIMARY KEY,
+    user_id    UUID NOT NULL,
+    exp        INT  NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
+
+CREATE TABLE "room_player"
+(
+    user_id    UUID NOT NULL,
+    room_id    UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (room_id) REFERENCES "room" (id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE "game"
+(
+    id         UUID PRIMARY KEY,
+    room_id    UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE "game_move"
+(
+    id         UUID PRIMARY KEY,
+    game_id    UUID  NOT NULL,
+    move       JSONB NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (game_id) REFERENCES "room" (id)
+);
